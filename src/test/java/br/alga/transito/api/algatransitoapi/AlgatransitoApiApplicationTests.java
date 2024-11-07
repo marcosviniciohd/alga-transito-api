8ccafba95f57d2fd348102afd48ca69091717341
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-06 22:09:33 -0300

    chore: commit estrutura inicial

---
+++ src/test/java/br/alga/transito/api/algatransitoapi/AlgatransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alga.transito.api.algatransitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgatransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
