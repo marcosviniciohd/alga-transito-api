41bb85cff83e10b126042a81350930ce5c35e957
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-23 20:15:12 -0300

    feat: alterado ProprietarioController para retornar proprietarios utilizando entityManager, add show sql.

--- src/main/java/br/alga/transito/api/algatransitoapi/controller/ProprietarioController.java
+++ src/main/java/br/alga/transito/api/algatransitoapi/controller/ProprietarioController.java
@@ -1,6 +1,8 @@
 package br.alga.transito.api.algatransitoapi.controller;
 
 import br.alga.transito.api.algatransitoapi.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +12,15 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
+    private final EntityManager manager;
+
+    public ProprietarioController(EntityManager manager) {
+        this.manager = manager;
+    }
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("João da Silva");
-        proprietario1.setEmail("joao.silva@gmail.com");
-        proprietario1.setTelefone("47 99999-9999");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria da Silva");
-        proprietario2.setEmail("maria.silva@gmail.com");
-        proprietario2.setTelefone("77 99999-9999");
-
-        return Arrays.asList(proprietario1, proprietario2);
-
+        return manager.createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 }
