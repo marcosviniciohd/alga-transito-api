409a2461e1dd2f44ee717fbe56520996c51ce8cd
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-23 19:50:52 -0300

    feat: add mapeamento orm classe Proprietario

--- src/main/java/br/alga/transito/api/algatransitoapi/domain/model/Proprietario.java
+++ src/main/java/br/alga/transito/api/algatransitoapi/domain/model/Proprietario.java
@@ -1,15 +1,28 @@
 package br.alga.transito.api.algatransitoapi.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
+
+
 
 
 }
