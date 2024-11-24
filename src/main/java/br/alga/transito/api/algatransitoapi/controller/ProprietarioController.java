121abd469bf7d9e72085e494c264284dbcc61ff6
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-11-24 15:32:36 -0300

    feat: inserir um novo recurso

--- src/main/java/br/alga/transito/api/algatransitoapi/controller/ProprietarioController.java
+++ src/main/java/br/alga/transito/api/algatransitoapi/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package br.alga.transito.api.algatransitoapi.controller;
 import br.alga.transito.api.algatransitoapi.domain.model.Proprietario;
 import br.alga.transito.api.algatransitoapi.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @AllArgsConstructor
@@ -17,7 +15,7 @@ public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
 
-
+    @GetMapping
     public List<Proprietario> listar() {
 
         // Consulta com like
@@ -35,5 +33,11 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
